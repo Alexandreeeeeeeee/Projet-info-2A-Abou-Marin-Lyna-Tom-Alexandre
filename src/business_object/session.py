6390229379b39fe8064a329543b12bbd60407261
LynaKh <kheloufi.lyna03@gmail.com>
2024-09-27 16:29:56 +0200

    business lyna

---
+++ src/business_object/session.py
@@ -0,0 +1,23 @@
+from business_object.utilisateur import Utilisateur
+
+# business_object/session.py
+
+class Session:
+    def __init__(self, sessionID, ts, auth, level, userAgent, item_in_session, userID_id):
+        self.sessionID = sessionID
+        self.ts = ts
+        self.auth = auth
+        self.level = level
+        self.userAgent = userAgent
+        self.item_in_session = item_in_session
+        self.userID_id = userID_id
+
+# Ajoutez la classe SessionDAO ici
+class SessionDAO:
+    def __init__(self):
+        # Initialisez votre connexion à la base de données ici
+        pass
+
+    def get_user_by_id(self, sessionID):
+        # Implémentez la logique pour récupérer une session par son ID
+        pass
