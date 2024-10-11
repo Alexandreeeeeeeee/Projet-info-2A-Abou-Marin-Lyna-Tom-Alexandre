9c864a97f4ff322006a7da97d0a00ff1b494afa4
unknown <tomcatoire9@gmail.com>
2024-10-11 16:24:06 +0200

    mod

--- src/dao/db_connection.py
+++ src/dao/db_connection.py
@@ -6,12 +6,13 @@ import os
 # Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
 
+
 def get_connection():
     conn = psycopg2.connect(
-        dbname=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT')
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
     )
     return conn
