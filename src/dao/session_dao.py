da1959a685e8fe341ce4606b37891841f1d2ad23
unknown <tomcatoire9@gmail.com>
2024-10-22 15:01:13 +0200

    tests user song

--- src/dao/session_dao.py
+++ src/dao/session_dao.py
@@ -1,15 +1,17 @@
 from dao.db_connection import get_connection
 from business_object.session import Session
 
+
 class SessionDAO:
     from dao.db_connection import get_connection
+
+
 from business_object.session import Session
+
 
 class SessionDAO:
     def __init__(self):
         self.connection = get_connection()
-
-
 
     def add_session(self, session):
         conn = get_connection()
@@ -17,18 +19,21 @@ class SessionDAO:
         query = """
             INSERT INTO analytics_session ("sessionID", "ts", "auth", "level", "userAgent", "item_in_session", "userID_id")
             VALUES (%s, %s, %s, %s, %s, %s, %s)
-            
+
         """
         try:
-            cursor.execute(query, (
-                session.sessionID,
-                session.ts,
-                session.auth,
-                session.level,
-                session.userAgent,
-                session.item_in_session,
-                session.userID_id
-            ))
+            cursor.execute(
+                query,
+                (
+                    session.sessionID,
+                    session.ts,
+                    session.auth,
+                    session.level,
+                    session.userAgent,
+                    session.item_in_session,
+                    session.userID_id,
+                ),
+            )
             conn.commit()
         except Exception as e:
             print(f"Error inserting session: {e}")
@@ -59,7 +64,9 @@ class SessionDAO:
     def calculate_average_session_duration(self):
         with self.connection.cursor() as cursor:
             try:
-                cursor.execute("SELECT AVG(ts) FROM analytics_session")  # Supposons que 'ts' représente la durée
+                cursor.execute(
+                    "SELECT AVG(ts) FROM analytics_session"
+                )  # Supposons que 'ts' représente la durée
                 return cursor.fetchone()[0]  # Renvoie la durée moyenne des sessions
             except Exception as e:
                 print(f"Erreur lors du calcul de la durée moyenne des sessions : {e}")
